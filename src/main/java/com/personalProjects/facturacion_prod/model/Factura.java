660a37e129a270e25cb1ed370cb8c4270e2f5fad
Itzel Velazquez <karinalugovelazquez@gmail.com>
2025-09-18 13:29:16 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/personalProjects/facturacion_prod/model/Factura.java
@@ -0,0 +1,31 @@
+package com.personalProjects.facturacion_prod.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name= "facturas")
+@Getter
+@Setter
+public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String numeroFactura;
+    private BigDecimal subtotal;
+    private BigDecimal total;
+
+    @Transient
+    private final double IVA = 0.15; //15%
+    private LocalDateTime fechaCreado;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "factura", orphanRemoval = true)
+    private Set<DetalleFactura> detalleFacturas;
+
+}
---
+++ src/main/java/com/personalProjects/facturacion_prod/repository/FacturaRepository.java
@@ -0,0 +1,8 @@
+package com.personalProjects.facturacion_prod.repository;
+
+import com.personalProjects.facturacion_prod.model.DetalleFactura;
+import com.personalProjects.facturacion_prod.model.Factura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacturaRepository extends JpaRepository <Factura, Integer>{
+}
